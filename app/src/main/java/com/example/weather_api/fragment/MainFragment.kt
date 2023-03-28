047328092c67d14320e13e1924b7622d7c5ec50c
pavel.hnelytsia <pavelgnelicya@gmail.com>
2023-03-28 17:46:20 +0300

    Adapter for switching Fragments

--- app/src/main/java/com/example/weather_api/fragment/MainFragment.kt
+++ app/src/main/java/com/example/weather_api/fragment/MainFragment.kt
@@ -9,7 +9,10 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.FragmentActivity
+import com.example.weather_api.adapters.VpAdapter
 import com.example.weather_api.databinding.FragmentMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 
 /**
@@ -18,6 +21,14 @@ import com.example.weather_api.databinding.FragmentMainBinding
  * @constructor Create empty Main fragment
  */
 class MainFragment : Fragment() {
+    private val fList = listOf(
+        HoursFragment.newInstance(),
+        DaysFragment.newInstance()
+    )
+    private val tList = listOf(
+        "HOURS",
+        "DAYS"
+    )
     private lateinit var binding: FragmentMainBinding
     private lateinit var pLauncher: ActivityResultLauncher<String>
 
@@ -35,6 +46,16 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         checkPermission()
+        init()
+
+    }
+
+    private fun init() = with(binding) {
+        val adapter = VpAdapter(activity as FragmentActivity, fList)
+        vp.adapter = adapter
+        TabLayoutMediator(tabLayout,vp){
+            tab,pos -> tab.text = tList[pos]
+        }.attach()
     }
 
     /**
@@ -55,6 +76,7 @@ class MainFragment : Fragment() {
             pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         }
     }
+
     companion object {
         @JvmStatic
         fun newInstance() = MainFragment()
