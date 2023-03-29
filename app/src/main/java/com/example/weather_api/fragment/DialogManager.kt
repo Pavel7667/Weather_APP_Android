d24d3cb01e8a4e8f7c09ffa58bd7b64a17be1c17
pavel.hnelytsia <pavelgnelicya@gmail.com>
2023-03-29 21:24:09 +0300

    DialogManager add some fun for search

--- app/src/main/java/com/example/weather_api/fragment/DialogManager.kt
+++ app/src/main/java/com/example/weather_api/fragment/DialogManager.kt
@@ -2,18 +2,40 @@ package com.example.weather_api.fragment
 
 import android.app.AlertDialog
 import android.content.Context
+import android.widget.EditText
 
 /**
  * Object with fun that will be asking user for GPS activate
  */
 object DialogManager {
+    /**
+     * Add refresh Location
+     */
     fun locationSettingDialog(context: Context,listener: Listener) {
         val builder = AlertDialog.Builder(context)
         val dialog = builder.create()
         dialog.setTitle("Enable location ? ")
         dialog.setMessage("Activate in parameters ? ")
         dialog.setButton(AlertDialog.BUTTON_POSITIVE,"OK"){_,_->
-            listener.onClick()
+            listener.onClick(null)
+            dialog.dismiss()
+        }
+        dialog.setButton(AlertDialog.BUTTON_NEGATIVE,"Cancel"){_,_->
+            dialog.dismiss()
+        }
+        dialog.show()
+    }
+    /**
+     * Add search function
+     */
+    fun searchByNameDialog(context: Context, listener: Listener) {
+        val builder = AlertDialog.Builder(context)
+        val edName = EditText(context)
+        builder.setView(edName)
+        val dialog = builder.create()
+        dialog.setTitle("Chose your City:")
+        dialog.setButton(AlertDialog.BUTTON_POSITIVE,"OK"){_,_->
+            listener.onClick(edName.text.toString())
             dialog.dismiss()
         }
         dialog.setButton(AlertDialog.BUTTON_NEGATIVE,"Cancel"){_,_->
@@ -26,6 +48,6 @@ object DialogManager {
      *In case BUTTON_POSITIVE do onClick
      */
     interface Listener{
-        fun onClick()
+        fun onClick(name:String?)
     }
 }
