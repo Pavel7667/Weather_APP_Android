1b1d62b7b865c768122d3da13e567af988951ef4
pavel.hnelytsia <pavelgnelicya@gmail.com>
2023-03-29 18:44:56 +0300

    add in HoursFragment

--- app/src/main/java/com/example/weather_api/fragment/HoursFragment.kt
+++ app/src/main/java/com/example/weather_api/fragment/HoursFragment.kt
@@ -1,19 +1,25 @@
 package com.example.weather_api.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.weather_api.MainViewModel
 import com.example.weather_api.adapters.WeatherAdapter
 import com.example.weather_api.adapters.WeatherModel
 import com.example.weather_api.databinding.FragmentHoursBinding
+import org.json.JSONArray
+import org.json.JSONObject
 
 class HoursFragment : Fragment() {
 
     private lateinit var binding: FragmentHoursBinding
-    private lateinit var  adapter: WeatherAdapter
+    private lateinit var adapter: WeatherAdapter
+    private val model: MainViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,23 +35,40 @@ class HoursFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initRcView()
+        model.liveDataCurrent.observe(viewLifecycleOwner) {
+            adapter.submitList(getHoursList(it))
+        }
     }
+
     /**
      * Push elements from LIST to Hours Fragment
      */
-    private fun initRcView()=with(binding){
-        rcView.layoutManager =LinearLayoutManager(activity)
-        adapter=WeatherAdapter()
+    private fun initRcView() = with(binding) {
+        rcView.layoutManager = LinearLayoutManager(activity)
+        adapter = WeatherAdapter()
         rcView.adapter = adapter
-        val list = listOf(
-            WeatherModel("dnepr","10:00","Sunny","25",
-                "","","",""),
-            WeatherModel("dnepr","12:00","Sunny","20",
-                "","","",""),
-            WeatherModel("dnepr","14:00","Cloudy","18",
-                "","","",""),
-        )
-        adapter.submitList(list)
+
+    }
+    /**
+     * Push INFO from JSON to VIEW
+     */
+    private fun getHoursList(wItem: WeatherModel): List<WeatherModel> {
+        val hoursArray = JSONArray(wItem.hours)
+        val list = ArrayList<WeatherModel>()
+        for (i in 0 until hoursArray.length()) {
+            val item = WeatherModel(
+                wItem.city,
+                (hoursArray[i] as JSONObject).getString("time"),
+                (hoursArray[i] as JSONObject).getJSONObject("condition").getString("text"),
+                (hoursArray[i] as JSONObject).getString("temp_c"),
+                "",
+                "",
+                (hoursArray[i] as JSONObject).getJSONObject("condition").getString("icon"),
+                ""
+            )
+            list.add(item)
+        }
+        return list
     }
 
     companion object {
