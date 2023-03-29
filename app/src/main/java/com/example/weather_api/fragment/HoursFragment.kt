58c3965aa9e1dd1212e40cf3031bfeb47342f24d
pavel.hnelytsia <pavelgnelicya@gmail.com>
2023-03-29 10:40:16 +0300

    HoursFragment push elements from List to View

--- app/src/main/java/com/example/weather_api/fragment/HoursFragment.kt
+++ app/src/main/java/com/example/weather_api/fragment/HoursFragment.kt
@@ -1,20 +1,51 @@
 package com.example.weather_api.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.weather_api.R
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.weather_api.adapters.WeatherAdapter
+import com.example.weather_api.adapters.WeatherModel
+import com.example.weather_api.databinding.FragmentHoursBinding
 
 class HoursFragment : Fragment() {
+
+    private lateinit var binding: FragmentHoursBinding
+    private lateinit var  adapter: WeatherAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hours, container, false)
+        binding = FragmentHoursBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    /**
+     * Push elements to Hours Fragment
+     */
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRcView()
+    }
+    /**
+     * Push elements from LIST to Hours Fragment
+     */
+    private fun initRcView()=with(binding){
+        rcView.layoutManager =LinearLayoutManager(activity)
+        adapter=WeatherAdapter()
+        rcView.adapter = adapter
+        val list = listOf(
+            WeatherModel("dnepr","10:00","Sunny","25",
+                "","","",""),
+            WeatherModel("dnepr","12:00","Sunny","20",
+                "","","",""),
+            WeatherModel("dnepr","14:00","Cloudy","18",
+                "","","",""),
+        )
+        adapter.submitList(list)
     }
 
     companion object {
