b01fe6c26e6705fd28d976ee7c10050b90540ad6
pavel.hnelytsia <pavelgnelicya@gmail.com>
2023-03-29 20:27:41 +0300

    MainFragment add AUTO-Location: link asking on init fun getLocation(latitude,longitude)

--- app/src/main/java/com/example/weather_api/fragment/MainFragment.kt
+++ app/src/main/java/com/example/weather_api/fragment/MainFragment.kt
@@ -1,6 +1,7 @@
 package com.example.weather_api.fragment
 
 import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -10,6 +11,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.activityViewModels
 import com.android.volley.Request
@@ -19,9 +21,14 @@ import com.example.weather_api.MainViewModel
 import com.example.weather_api.adapters.VpAdapter
 import com.example.weather_api.adapters.WeatherModel
 import com.example.weather_api.databinding.FragmentMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.CancellationTokenSource
 import com.google.android.material.tabs.TabLayoutMediator
 import com.squareup.picasso.Picasso
 import org.json.JSONObject
+import kotlin.coroutines.cancellation.CancellationException
 
 const val API_KEY = "8af04a7d0ff243f59f882715232803"
 
@@ -32,6 +39,8 @@ const val API_KEY = "8af04a7d0ff243f59f882715232803"
  * @constructor Create empty Main fragment
  */
 class MainFragment : Fragment() {
+    private lateinit var fLocationClient: FusedLocationProviderClient
+
     // List of Fragments
     private val fList = listOf(
         HoursFragment.newInstance(),
@@ -63,18 +72,42 @@ class MainFragment : Fragment() {
         checkPermission()
         init()
         updateCurrentCard()
-        requestWeatherData("Dnipropetrovsk")
+        getLocation()
     }
 
     /**
      * Fun Init to switching between Fragments
      */
     private fun init() = with(binding) {
+        fLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
         val adapter = VpAdapter(activity as FragmentActivity, fList)
         vp.adapter = adapter
         TabLayoutMediator(tabLayout, vp) { tab, pos ->
             tab.text = tList[pos]
         }.attach()
+        idSync.setOnClickListener{
+            tabLayout.selectTab(tabLayout.getTabAt(0))
+            getLocation()
+        }
+    }
+
+    private fun getLocation() {
+        val ct = CancellationTokenSource()
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        fLocationClient
+            .getCurrentLocation(LocationRequest.PRIORITY_HIGH_ACCURACY, ct.token)
+            .addOnCompleteListener {
+                requestWeatherData("${it.result.latitude},${it.result.longitude}")
+            }
     }
 
     private fun updateCurrentCard() = with(binding) {
